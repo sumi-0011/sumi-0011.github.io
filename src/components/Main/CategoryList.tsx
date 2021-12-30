686f5ec6b3c2dfea76bb536619e8d0d6d2faf9c2
sumi.byun <selina2000@naver.com>
2021-12-30 15:42:22 +0900

    카테고리 목록 컴포넌트 구현

---
+++ src/components/Main/CategoryList.tsx
@@ -0,0 +1,68 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+
+export type CategoryListProps = {
+  selectedCategory: string
+  categoryList: {
+    // 프로퍼티 이름은 문자열, 프로퍼티 값은 숫자임을 나타내는 타입 표기 방법
+    [key: string]: number
+  }
+}
+// CategoryItem 컴포넌트에서 사용할 Props
+type CategoryItemProps = {
+  active: boolean
+}
+// Link 컴포넌트에 전달해주기 위한 Props
+// & 연산자를 통해 Intersection 타입을 정의하여 두 타입을 하나로 합침
+type GatsbyLinkProps = {
+  children: ReactNode
+  className?: string
+  to: string
+} & CategoryItemProps
+
+const CategoryListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 768px;
+  margin: 100px auto 0;
+`
+// Query Parameter로 받은 category 이름과 동일한 아이템의 폰트를 더 굵게 표시
+// active : 현재 선택된 카테고리인지 확인하기 위한 Props
+// children : 컴포넌트 내부의 요소들
+// className : Styled Component를 통해 정의한 스타일을 적용하기 위한 클래스명
+// to : Link 컴포넌트에 전달하기 위한 경로
+const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
+  <Link {...props} />
+))<CategoryItemProps>`
+  margin-right: 20px;
+  padding: 5px 0;
+  font-size: 18px;
+  font-weight: ${({ active }) => (active ? '800' : '400')};
+  cursor: pointer;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`
+const CategoryList: FunctionComponent<CategoryListProps> = function ({
+  selectedCategory,
+  categoryList,
+}) {
+  return (
+    <CategoryListWrapper>
+      {/* Object.entries() 메서드는 for...in와 같은 순서로 주어진 객체 자체의 enumerable 속성 [key, value] 쌍의 배열을 반환합니다 => 배열을 반환하기 때문에 map메서드를 호출할 수 있다. */}
+      {Object.entries(categoryList).map(([name, count]) => (
+        <CategoryItem
+          to={`/?category=${name}`}
+          active={name === selectedCategory}
+          key={name}
+        >
+          #{name}({count})
+        </CategoryItem>
+      ))}
+    </CategoryListWrapper>
+  )
+}
+
+export default CategoryList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,25 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
-import Introduction from 'components/Main/Introduction'
 import Footer from 'components/Common/Footer'
-
+import Introduction from 'components/Main/Introduction'
+import CategoryList from 'components/Main/CategoryList'
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
 `
-
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 const IndexPage: FunctionComponent = function () {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
   )
