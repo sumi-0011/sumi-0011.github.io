49a1e2666f1e750fd5b942ca9d65b56d51bbdbab
sumi.byun <selina2000@naver.com>
2021-12-30 15:16:59 +0900

    Footer 생성
    footer가 맨 밑으로 가게 하기 위하여 height를 100vh로 지정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Introduction from 'components/Main/Introduction'
+import Footer from 'components/Common/Footer'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `
 
 const IndexPage: FunctionComponent = function () {
@@ -14,6 +15,7 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <Footer />
     </Container>
   )
 }
