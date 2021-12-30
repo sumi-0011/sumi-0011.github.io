f1c66104c42af487c64f70e71b9fc8ceb55f0cb3
sumi.byun <selina2000@naver.com>
2021-12-30 15:03:47 +0900

    emojs test

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,33 +1,5 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import styled from '@emotion/styled'
-import { Global, css } from '@emotion/react'
-const globalStyle = css`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-
-    font-size: 20px;
-  }
-`
-
-const TextStyle = css`
-  font-size: 18px;
-  font-weight: 700;
-  color: gray;
-`
-const Text1 = styled.div<{ disable: boolean }>`
-  font-size: 20px;
-  font-weight: 700;
-  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
-`
-
-const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
-  fontSize: '15px',
-  color: 'blue',
-  textDecoration: disable ? 'line-through' : 'none',
-}))
 
 type InfoPageProps = {
   data: {
@@ -50,10 +22,7 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <Global styles={globalStyle} />
-      <div css={TextStyle}>{title}</div>
-      <Text1 disable={true}>{description}</Text1>
-      <Text2 disable={true}>{author}</Text2>
+      {title} {description} {author}
     </div>
   )
 }
