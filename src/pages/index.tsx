2a6557e057a143e9fb09d60b138faed68042a797
sumi.byun <selina2000@naver.com>
2022-01-03 09:38:21 +0900

    post item  링크 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -95,6 +95,9 @@ export const getPostList = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary
